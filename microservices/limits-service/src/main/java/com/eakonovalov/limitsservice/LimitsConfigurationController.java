20c8ce70052d3dbf983b86f6c222c9f2b4640159
Evgeny Konovalov <eakonovalov@gmail.com>
2018-11-01 17:13:44 +0300

    hystrix

--- microservices/limits-service/src/main/java/com/eakonovalov/limitsservice/LimitsConfigurationController.java
+++ microservices/limits-service/src/main/java/com/eakonovalov/limitsservice/LimitsConfigurationController.java
@@ -1,6 +1,7 @@
 package com.eakonovalov.limitsservice;
 
 import com.eakonovalov.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,16 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfigurations() {
         return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfigurations")
+    public LimitConfiguration retrieveConfigurations() {
+        throw new RuntimeException("Not available");
+    }
+
+    public LimitConfiguration fallbackRetrieveConfigurations() {
+        return new LimitConfiguration(9, 9999);
     }
 
 }
