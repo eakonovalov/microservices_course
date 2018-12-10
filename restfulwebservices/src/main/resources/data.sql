e72c8607045f14e8a72b2b4bb865b068115a3832
Evgeny Konovalov <eakonovalov@gmail.com>
2018-12-11 00:24:16 +0300

    fix

--- restfulwebservices/src/main/resources/data.sql
+++ restfulwebservices/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into user  values (101, sysdate(), 'AB');
-insert into user  values (102, sysdate(), 'Jill');
-insert into user  values (103, sysdate(), 'Jam');
+insert into user values (101, sysdate(), 'AB');
+insert into user values (102, sysdate(), 'Jill');
+insert into user values (103, sysdate(), 'Jam');
 
-insert into post  values (101, 'My First Post', 101);
-insert into post  values (102, 'My Second Post', 101);
+insert into post values (101, 'My First Post', 101);
+insert into post values (102, 'My Second Post', 101);
